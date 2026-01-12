234b5b4360b25734902b5495717070088f1c0f35
Guillaume Riou <griou@sat.qc.ca>
2026-01-12 12:01:41 -0500

    commit everything

--- scripts/push-puara-arduino.sh
+++ scripts/push-puara-arduino.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -eux
 
-PUARA_MODULE_SOURCES=(
-  src data
-  .clang-format .gitignore library.properties
-  LICENSE README.md
-)
-
 (
   cd puara-arduino || exit 1
 
@@ -14,8 +8,7 @@ PUARA_MODULE_SOURCES=(
 
   git checkout -b continuous
 
-  git add "${PUARA_MODULE_SOURCES[@]}" examples
-  git commit -m "$GITHUB_REF :: $GITHUB_SHA"
+  git commit -am "$GITHUB_REF :: $GITHUB_SHA"
 
   git push --set-upstream origin continuous --force
 )
