7470d0574dce3fa553780748e21ad2480d8951d3
Guillaume Riou <griou@sat.qc.ca>
2026-01-12 15:10:40 -0500

    uncomment tests, disable debug push task, formatting

--- scripts/push-puara-arduino.sh
+++ scripts/push-puara-arduino.sh
@@ -1,23 +1,16 @@
 #!/bin/bash -eux
 
-(
-  cd puara-arduino || exit 1
+cd puara-arduino || exit 1
 
-  git config user.name "puara-bot"
-  git config user.email "puara-bot@sat.qc.ca"
+git config user.name "puara-bot"
+git config user.email "puara-bot@sat.qc.ca"
 
-
-  git status
-
-  # looks to see if there is something to commit.
-  if [ `git status --porcelain=1 | wc -l` -eq 0 ]; then
-      echo "Nothing to commit."
-      exit 0
-  else
-      # git add src/* data/* examples/* LICENSE README.md library.properties .clang-format .gitignore --ignore-errors
-      git add -A
-      git commit -am "$GITHUB_REF :: $GITHUB_SHA"
-      git push --set-upstream origin continuous --force
-  fi
-
-)
+# looks to see if there is something to commit.
+if [ `git status --porcelain=1 | wc -l` -eq 0 ]; then
+    echo "Nothing to commit."
+    exit 0
+else
+    git add -A
+    git commit -am "$GITHUB_REF :: $GITHUB_SHA"
+    git push --set-upstream origin continuous --force
+fi
