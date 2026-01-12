69ed1f3796ba2bc5ebb930a58642b35be3f69dfe
Guillaume Riou <griou@sat.qc.ca>
2026-01-12 11:52:46 -0500

    checkout the repository

--- scripts/push-puara-arduino.sh
+++ scripts/push-puara-arduino.sh
@@ -8,6 +8,12 @@ PUARA_MODULE_SOURCES=(
 
 (
   cd puara-arduino || exit 1
+
+  git config user.name "puara-bot"
+  git config user.email "puara-bot@sat.qc.ca"
+
+  git checkout -b continuous
+
   git add "${PUARA_MODULE_SOURCES[@]}" examples
   git commit -m "$GITHUB_REF :: $GITHUB_SHA"
 
