1adae398f570e232c585d78f3202b2abf1049e7b
Charles B <bicari.charles@gmail.com>
2026-02-18 15:27:49 -0500

    WIP : first beacon based FTM query succes :)

--- src/puara.h
+++ src/puara.h
@@ -59,34 +59,11 @@ public:
 
   const std::vector<PuaraAPI::FTM::scanned_responder_ap_info>& get_scanned_responder_aps() const;
 
+  bool ftm_report_available();
+
 //  void FTM::configureFTM(uint8_t frame_count, uint16_t burst_period, uint8_t* target_bssid, uint8_t target_channel){
 
-  /**
-   * Trigger an FTM procedure and get the report in the event handler. Call this only on devices acting as FTM initiators (STAs connected to a responder AP). Do NOT call this on devices acting as solely as FTM responders (APs).
-   */
 
-//  void requestFTM();
-
-  // Get last distance estimate in cm from FTM report
-  uint32_t get_last_distance_cm();
-  
-  // Get last RTT estimate in ns from FTM report
-  uint32_t get_last_rtt_ns();
-
-  // Verify if FTM report data is available
-  bool is_ftm_report_available();
-
-  // Mark FTM report as consumed after reading the data to prevent stale data usage
-  void set_ftm_report_as_consumed();
-/*
-  // Set offset in cm for FTM Responder.
-  bool set_offset_responder(int16_t offset_cm);
-
-  // End the ongoing FTM Initiator session.
-  void end_ftm_request_session();
-*/
-  // Get RSSI of last FTM frame from connected external AP
-  int get_rssi_of_ftm_frame();
 
   double getVarNumber(std::string varName);
   std::string getVarText(std::string varName);
--- src/puara_ftm.hpp
+++ src/puara_ftm.hpp
@@ -26,9 +26,18 @@ struct FTM
     int8_t rssi;
     uint8_t primary_channel;
   };
-
+/*
+  struct ftm_report_info
+  {
+    uint32_t rtt_est; // in nanoseconds
+    uint32_t dist_est; // in centimeters
+    int8_t rssi; // RSSI of the FTM frame, as an indicator of signal strength of the FTM responder AP
+  };
+*/
+   /**
+    * Configure and trigger an FTM procedure, with parameters defined in wifi_ftm_cfg. Get the FTM report data in the event handler (sta_event_handler in WiFi module) when the procedure is completed. Call this only on devices acting as FTM initiators (STAs connected to a responder AP). Do NOT call this on devices acting as solely as FTM responders (APs).
+    */
 // as the objective is trilateration, qudrilateration, or more, users should have a struct for ftm configurations which is a definable array in the setup loop
-
   std::vector<scanned_responder_ap_info> scanned_responder_aps; // Array to hold scanned AP info for FTM purposes, populated during WiFi scan
 
   uint32_t last_distance_cm = 0; // last distance estimate in cm from FTM
@@ -36,13 +45,9 @@ struct FTM
   bool ftm_report_available = false; // flag to indicate if FTM report data is available  
   int rssi_of_ftm_frame = 0;
 
-  wifi_ftm_initiator_cfg_t wifi_ftm_cfg{};
+  std::vector<wifi_ftm_initiator_cfg_t> wifi_ftm_cfgs; // Array to hold FTM configuration for multiple APs, to be defined by user before calling requestFTM()
 
-  /*
-//  void configureFTM(uint8_t frm_cnt = 32, uint16_t burst_prd = 4); // Configure FTM parameters
-  void requestFTM(); 
-  bool set_offset_responder(int16_t offset_cm); // Set offset in cm for FTM Responder
-  void end_ftm_request_session(); 
-  */
+//  void make_vector_FTM_configurations(uint8_t frame_count, uint16_t burst_period);
+
 };
 } 
